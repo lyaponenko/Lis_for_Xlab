dedb209ad7ed0f3cdb8df064fcb706da2b28d770
Ilya Lyaponenko <i.lyaponenko@carx-tech.com>
2025-05-07 12:02:12 +0300

    unit 3

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TZ2
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject prefab;
+        public void Spawn()
+        {
+            Debug.Log("Spawn");
+            if (prefab == null)
+            {
+                Debug.LogError("Spawner - prefab==null");
+                return;
+            }
+            Instantiate(prefab, transform.position, Quaternion.identity);
+        }
+    }
+}
