731ed70e3cb0d5a4aa537cb8b99feb3f64bece1f
Ilya Lyaponenko <i.lyaponenko@carx-tech.com>
2025-05-12 15:40:25 +0300

    base scripts

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TZ2
+namespace Test
 {
     public class Cloud : MonoBehaviour
     {
         [SerializeField] private ParticleSystem m_particleSystem;
 
         public void PlayFX()
-        {
+        { 
             m_particleSystem.Play();
         }
+
         public void StopFX()
-        {
+        { 
             m_particleSystem.Stop();
         }
-        public void Start()
+
+        private void Start()
         {
             m_particleSystem.Stop();
         }
--- Assets/Scripts/CloudController.cs
+++ Assets/Scripts/CloudController.cs
@@ -1,22 +1,24 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-namespace TZ2
+namespace Test
 {
     public class CloudController : MonoBehaviour
     {
+        public Transform[] targets;
+
         private int m_targetIndex = 0;
         private bool m_moved = false;
-        public float moveSpeed = 15f;
+        public float moveSpeed = 0.001f;
         public Cloud cloud;
-        public Transform[] targets;
         public void Action()
         {
-            Debug.Log("Cloud", this);
+            Debug.Log("CloudCon - try", this);
 
             if (m_moved)
-            {
+            { 
                 return;
             }
             m_moved = true;
@@ -24,25 +26,21 @@ namespace TZ2
             m_targetIndex++;
             if (m_targetIndex >= targets.Length) { m_targetIndex = 0; }
         }
-        public void Update()
-        {
-            if (!m_moved)
-            { return; }
+        private void Update()
+        { 
+            if (!m_moved) { return; }
 
-            Transform target = targets[m_targetIndex]; 
+            Transform target = targets[m_targetIndex];
             Vector3 targetPosition = new Vector3(target.position.x, cloud.transform.position.y, target.position.z);
-            Vector3 offset = (targetPosition - cloud.transform.position).normalized * Time.deltaTime * moveSpeed;
-
+           //cloud.position = Vector3.Lerp(cloud.position, targetPosition, Time.deltaTime);
+            Vector3 offset = (targetPosition - cloud.transform.position).normalized * moveSpeed * Time.deltaTime;
             if (Vector3.Distance(cloud.transform.position, targetPosition) < 0.1f)
             {
                 cloud.transform.position = targetPosition;
                 m_moved = false;
                 cloud.PlayFX();
             }
-            else
-            {
-                cloud.transform.Translate(offset, Space.World);
-            }
+            else { cloud.transform.Translate(offset); }
         }
     }
 }
